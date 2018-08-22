3493be8051e127458751ef3a38a4aa7c48813302
ShivaRenukuntla <shivasurya.prasad4@gmail.com>
2018-08-22 09:27:08 +0530

    first commit

---
+++ src/Components/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1
+  };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button style={this.styles} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
